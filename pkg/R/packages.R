99ff4d4e6bce7c7248c5acaa5596ad6b255990c1
renaud <renaud@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-01 07:41:49 +0000

    Add examples for add_lib
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pkgmaker@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/packages.R
+++ pkg/R/packages.R
@@ -158,11 +158,42 @@ CRAN <- 'http://cran.r-project.org'
 #' 
 #' Prepend/append paths to the library path list, using \code{\link{.libPaths}}.
 #' 
+#' This function is meant to be more convenient than \code{.libPaths}, which requires 
+#' more writing if one wants to:
+#' \itemize{
+#' \item sequentially add libraries;
+#' \item append and not prepend new path(s);
+#' \item keep the standard user library in the search path.
+#' }
+#' 
 #' @param ... paths to add to .libPath
 #' @param append logical that indicates that the paths should be appended
 #' rather than prepended.
 #' 
 #' @export
+#' 
+#' @examples
+#' ol <- .libPaths()
+#' # called sequentially, .libPaths only add the last library
+#' show( .libPaths('.') )
+#' show( .libPaths(tempdir()) )
+#' # restore
+#' .libPaths(ol)
+#' 
+#' # .libPaths does not keep the standard user library
+#' show( .libPaths() ) 
+#' show( .libPaths('.') )
+#' # restore
+#' .libPaths(ol)
+#' 
+#' # with add_lib
+#' show( add_lib('.') )
+#' show( add_lib(tempdir()) )
+#' show( add_lib('..', append=TRUE) )
+#' 
+#' # restore 
+#' .libPaths(ol)
+#' 
 add_lib <- function(..., append=FALSE){
 	
 	p <- 
